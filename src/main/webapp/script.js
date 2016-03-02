6f232741fda2f1f71068e025300f405e1a277e4e
Tobias <tobias.klevenz@gmail.com>
2016-03-02 12:16:07 +0100

    better auth and picker loading, popup not blocked anymore

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,25 +1,25 @@
 /*
-	The MIT License (MIT)
-	
-	Copyright (c) 2016 Tobias Klevenz (tobias.klevenz@gmail.com)
-	
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-	
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-	
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
+    The MIT License (MIT)
+    
+    Copyright (c) 2016 Tobias Klevenz (tobias.klevenz@gmail.com)
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
 // The Browser API key obtained from the Google Developers Console.
@@ -36,25 +36,42 @@ var oauthToken;
 
 // Use the API Loader script to load google.picker and gapi.auth.
 function onApiLoad() {
-    document.getElementById('pick').addEventListener('click', authAndSetupPicker);
+    gapi.client.setApiKey(developerKey);
+    window.setTimeout(checkAuth, 1);
 }
 
-function authAndSetupPicker() {
-    gapi.load('auth', {
-        'callback': onAuthApiLoad
-    });
+function checkAuth() {
+    gapi.auth.authorize({
+        client_id: clientId,
+        scope: scope,
+        immediate: true
+    }, handleAuthResult);
+}
+
+function handleAuthResult(authResult) {
+    var pickButton = document.getElementById('pick');
+    if (authResult && !authResult.error) {
+        oauthToken = authResult.access_token;
+        pickButton.onclick = createPicker;
+        loadPicker();
+    } else {
+        pickButton.onclick = handleAuthClick;
+    }
+}
+
+function handleAuthClick(event) {
+    gapi.auth.authorize({
+        client_id: clientId,
+        scope: scope,
+        immediate: false
+    }, handleAuthResult);
+    return false;
+}
+
+function loadPicker() {
     gapi.load('picker', {
         'callback': onPickerApiLoad
     });
-}
-
-function onAuthApiLoad() {
-    window.gapi.auth.authorize({
-            'client_id': clientId,
-            'scope': scope,
-            'immediate': false
-        },
-        handleAuthResult);
 }
 
 function onPickerApiLoad() {
@@ -62,13 +79,7 @@ function onPickerApiLoad() {
     createPicker();
 }
 
-function handleAuthResult(authResult) {
-    if (authResult && !authResult.error) {
-        oauthToken = authResult.access_token;
-        console.log("expires_in: " + authResult.expires_in);
-        createPicker();
-    }
-}
+
 
 // Create and render a Picker object for picking user documents.
 function createPicker() {
@@ -84,15 +95,14 @@ function createPicker() {
 }
 
 /**
- * Export a file's content.
+ * Setup hidden form param fields
+ * Enable transform button and transformation params dialog
  *
  * @param {File} file Drive File instance.
- * @param {Function} callback Function to call when the request is complete.
  */
-function exportFile(file, callback) {
+function setupTransform(file) {
     if (file['exportLinks']['text/html']) {
         var accessToken = gapi.auth.getToken().access_token;
-        var xhr = new XMLHttpRequest();
         var exportLink = encodeURI(file['exportLinks']['text/html']);
 
         // setup form
@@ -102,32 +112,23 @@ function exportFile(file, callback) {
         document.getElementById('run_transform').disabled = false;
         document.getElementById('transform_params').style.display = "block";
     } else {
-        callback(null);
+        console.log(file);
     }
 }
 
-// A simple callback implementation.
+// get file Drive instance from picker
 function pickerCallback(data) {
-    var url = 'nothing';
-    if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
-        var doc = data[google.picker.Response.DOCUMENTS][0];
-        url = doc[google.picker.Document.URL];
-    }
-
     if (data.action === google.picker.Action.PICKED) {
         var id = data.docs[0].id;
-        console.log(id);
         var request = new XMLHttpRequest();
         request.open('GET', 'https://www.googleapis.com/drive/v2/files/' + id);
         request.setRequestHeader('Authorization', 'Bearer ' + oauthToken);
 
         request.addEventListener('load', function() {
-            var item = JSON.parse(request.responseText);
-            console.log(item);
+            var file = JSON.parse(request.responseText);
+            console.log(file);
 
-            exportFile(item, function(responseText) {
-                console.log(responseText);
-            });
+            setupTransform(file);
         });
 
         request.send();
